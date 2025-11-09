b0356ef7b9247ca4b675684750bffabc89d70cf1
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2025-11-09 23:18:00 +0300

    fix: fix exception for email

--- src/main/java/imbuy/backend/dto/RegisterRequest.java
+++ src/main/java/imbuy/backend/dto/RegisterRequest.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequest(
-        @NotBlank @Email String email,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Must be a valid email address") String email,
         @NotBlank String password,
         String username
 ) {}
